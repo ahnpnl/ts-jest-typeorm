1efce6018a71b86701fa2c2e246495071ae187ce
Ahn <anhpnnd@gmail.com>
2020-05-24 12:05:35 +0200

    First ocmmit

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+/** @typedef {import('ts-jest')} */
+/** @type {import('@jest/types').Config.InitialOptions} */
+module.exports = {
+  /**
+   * replace with preset: 'ts-jest/presets/js-with-ts' if you need to compile js/ts with ts-jest, see the list of presets
+   * here https://kulshekhar.github.io/ts-jest/user/config/
+   */
+  preset: 'ts-jest',
+};
---
+++ src/entity/User.spec.ts
@@ -0,0 +1,5 @@
+import { User } from './User';
+
+test('should work', () => {
+  expect(new User()).toBeDefined();
+})
